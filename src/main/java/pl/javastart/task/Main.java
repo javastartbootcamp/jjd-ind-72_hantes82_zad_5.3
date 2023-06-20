b91efa1b1cedf2cdf5efdd35d9e1b2e948cb635c
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-06-20 21:20:27 +0200

    Zadanie 5.3

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,25 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Product prod1 = new Product("Mleko", "Mleko bez laktozy", 2.31, "Nabiał");
+        Product prod2 = new Product("Wołowina", "Wołowina długodojrzewająca", 55.51, "Mięso");
+        Product prod3 = new Product("Pieluchy", "Pampers", 70.30, "Artykuły dziecięce");
+        Product prod4 = new Product("Miska", "Misa grilowa", 17.00, "Inne");
 
+        System.out.println(grossPrice(prod1));
+        System.out.println(grossPrice(prod2));
+        System.out.println(grossPrice(prod3));
+        System.out.println(grossPrice(prod4));
+
+    }
+
+    static double grossPrice(Product prod) {
+        double grossPrice = switch (prod.getCategory()) {
+            case "Nabiał" -> prod.getNetPrice() * 1.12;
+            case "Mięso" -> prod.getNetPrice() * 1.21;
+            case "Artykuły dziecięce" -> prod.getNetPrice() * 1.08;
+            default -> prod.getNetPrice() * 1.23;
+        };
+        return grossPrice;
     }
 }
---
+++ src/main/java/pl/javastart/task/Product.java
@@ -0,0 +1,48 @@
+package pl.javastart.task;
+
+public class Product {
+    private String name;
+    private String description;
+    private double netPrice;
+    private String category;
+
+    Product(String name, String description, double netPrice, String category) {
+        this.name = name;
+        this.description = description;
+        this.netPrice = netPrice;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getNetPrice() {
+        return netPrice;
+    }
+
+    void setNetPrice(double netPrice) {
+        this.netPrice = netPrice;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    void setCategory(String category) {
+        this.category = category;
+    }
+}
+
